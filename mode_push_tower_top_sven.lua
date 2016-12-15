d99e9f3f937a0d53b251fed79587c2d72dc3bde9
ThePianoDentist <jbknight07@gmail.com>
2016-12-15 01:21:12 +0000

    sven you ejid

---
+++ mode_push_tower_top_sven.lua
@@ -0,0 +1,13 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Johnny
+-- Date: 14/12/2016
+-- Time: 22:58
+-- To change this template use File | Settings | File Templates.
+--
+
+----------------------------------------------------------------------------------------------------
+
+function GetDesire()
+    return 1.0
+end
