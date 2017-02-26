729e47cad8487043b32440276537f87453f95b15
ThePianoDentist <jbknight07@gmail.com>
2017-02-26 16:17:26 +0000

    This is a revert

--- hero_funcs/pulling.lua
+++ hero_funcs/pulling.lua
@@ -10,49 +10,46 @@ function reset_pull_vars()
 end
 
 function CDOTA_Bot_Script:pull_camp(camp, timing, should_chain, pull_num)
-    print("self:NumQueuedActions()" .. tostring(self:NumQueuedActions()))
-    print(_G.state.pull_status)
-    if self:NumQueuedActions() ~= 0 then
-        return
-    end
-
-    if _G.state.pull_status == "init" then
-        self:ActionQueue_MoveToLocation(camp.pull_to) -- inefficient if going from zoning -> pull
-        _G.state.pull_status = "waiting"
-        return
-    end
+--    if pull_num == 0 and chain_pull ~= nil then
+--        return self:pull_camp(RAD_SAFE_HARD, 59, false, 1);
+--    end
 
     -- someone damaging the camp without pulling might break this?
-    if _G.state.pull_status == "waiting" and pull_num == 0 then
-        self:wait_for_pull_timing(timing, camp)
-        _G.state.pull_status = "delay"
-        return
-    end
-    if _G.state.pull_status == "delay" then
-        self:aggro_camp(camp)
-        _G.state.pull_status = "aggro"
-        return
-    end
-    if should_chain and self:time_to_chain_pull(camp) == true and camp.is_alive then
-        print ("Doing chain pull")
-        reset_pull_vars()
-        camp.is_alive = false -- TODO this isnt being updated properly
-        _G.state.current_mode = "chain_pull_hard"
-        return
-    end
-    if _G.state.pull_status == "aggro" then
-        self:attack_targets(camp, false)
-        _G.state.pull_status = "farming"
-        return
-    end
 
+    -- even after aggroing, if theyre fogged it reports velocity as 0 and max health as -1. Nice! :D
+    if not _G.state.temp_memory.creeps_aggroed then
+        -- The only time our 'action' goes over the minute, is in a chain pull. if we've told it to chain pull, then dont need to check time
+        if (_G.seconds > timing - self:estimate_travel_time(camp.location) or pull_num == 1) then
+            self:aggro_camp(camp)
+        else
+            self:Action_MoveToLocation(camp.pull_to) -- now is too early. stand in waiting spot
+        end
+    else -- we've 'initiated' the pull
+        -- do the double pull
+        -- replace with time to pull and creep health to always work
+        if should_chain and self:time_to_chain_pull(camp) == true and camp.is_alive then
+            print ("Doing chain pull")
+            reset_pull_vars()
+            camp.is_alive = false -- TODO this isnt being updated properly
+            _G.state.current_mode = "chain_pull_hard"
+            return
+        end
+
+        --if pull_num == 1 then friendly_creep_rad_check = 400 -- because we want to pull creeps all way over to other camp. dont stop halfway
+        if _G.state.temp_memory.have_pulled ~= true and GetUnitToLocationDistance(self, camp.pull_to) > 200 then -- we have pulled and are dragging creeps into lane
+            self:Action_MoveToLocation(camp.pull_to)
+        else
+            _G.state.temp_memory.have_pulled = true
+            self:farm_camp(camp)
+        end
+    end
 end
 
 function CDOTA_Bot_Script:get_target(camp)
     if _G.state.current_target == nil or _G.state.current_target:IsAlive() == false then
         local neuts = self:GetNearbyNeutralCreeps(1000)
         for k, v in pairs(neuts) do
-            if (v:GetHealth() > 0) and GetUnitToLocationDistance(v, camp.location) < 300 then
+            if (v:GetHealth() > 0) and GetUnitToLocationDistance(v, camp.location) < 300 and string.match(v:GetUnitName(), "neutral") then
                 print ("Got new target")
                 return v
             end
@@ -63,28 +60,27 @@ function CDOTA_Bot_Script:get_target(camp)
     end
 end
 
-function CDOTA_Bot_Script:attack_targets(camp, once)
-    local neuts = self:GetNearbyNeutralCreeps(1000)
-    for k, v in pairs(neuts) do
-        if (v:GetHealth() > 0) and GetUnitToLocationDistance(v, camp.location) < 300 then
-            self:ActionQueue_AttackUnit(v, once)
-            if once then return end  -- only attack one creep
-        end
-    end
-    return nil
-end
-
-
 function CDOTA_Bot_Script:aggro_camp(camp)
-    self:attack_targets(camp, true)
-    self:ActionQueue_MoveToLocation(camp.pull_to)
-    _G.state.temp_memory.creeps_aggroed = true
-    return
-end
+    if _G.state.current_target == nil then
+         _G.state.current_target = self:get_target(camp)
+    end
 
-function CDOTA_Bot_Script:wait_for_pull_timing(timing, camp)
-    local delay = timing - self:estimate_travel_time(camp.location) - _G.seconds
-    self:ActionQueue_Delay(delay)
+    if _G.state.current_target ~= nil and _G.state.current_target:IsAlive() == true
+    then
+        print("Attack target")
+        if self:NumQueuedActions() == 0 then
+            self:ActionPush_AttackUnit(_G.state.current_target, true)
+            self:ActionQueue_MoveToLocation(camp.pull_to)
+        end
+
+        --_G.state.temp_memory.creeps_aggroed = true
+        if self:haveWeAggroedPull() then
+            _G.state.temp_memory.creeps_aggroed = true
+            print ("Have Aggored the creeps")
+        end
+    else
+        self:Action_MoveToLocation(camp.location)
+    end
     return
 end
 
